02c8e3b5d0e6f7d5799b2816018f37da33622528
prathyusha <142219850+ushaprathyusha@users.noreply.github.com>
2023-12-20 20:28:49 +0530

    hi

--- routes/invoicestockRoutes.js
+++ routes/invoicestockRoutes.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const router = express.Router();
 const {
+  getHSNCode,
   addInvoice,
   getInvoices,
   updateInvoice,
@@ -13,35 +14,17 @@ const {
   getMedicineDetails,
   getMedicineOnly, 
   updatePharmaQuantity,
-  getInvoiceDetails,
-  getBatchNumbers,
-  getMedicineDetailss,
-  getBatchDetails,
-  
+} = require("../Controllers/DrugMasterController");
 
-} = require("../Controllers/invoicestockController");
 // Add Invoice
-
-router.get('/batchDetails/:batchNumber', getBatchDetails);
-router.get('/batchNumbers', getBatchNumbers);
-
-const { getInvoiceNumbers } = require("../Controllers/invoicestockController");
-router.get("/invoiceDetails/:selectedInvoice", getInvoiceDetails);
-// Add a route to fetch invoice numbers
-router.get("/getInvoiceNumbers", getInvoiceNumbers);
-router.get("/medicineDetails/:medicineName", getMedicineDetails);
-
 router.post("/addInvoice", addInvoice);
 
-router.get("/medicineDetails/:medicineName", getMedicineDetails);
-
-router.get("/api/getInvoiceNumbers",getInvoiceNumbers )
-// Get All Invoices
+// Get All Invoices  
 router.get("/getInvoices", getInvoices);
 
 router.get("/medicines", getMedicineOnly); 
 
-
+router.get("/medicines/${selectedMedicine}/hsn", getHSNCode);
 
 // Update Invoice
 router.put("/updateInvoice/:invoiceId", updateInvoice);
