aaf0e673f853521db491891ca44034f2ab706e97
vikas90322 <132671498+vikas90322@users.noreply.github.com>
2023-12-22 18:04:16 +0530

    hiiii

---
+++ models/registrationModel.js
@@ -0,0 +1,12 @@
+// models/registrationModel.js
+const mongoose = require('mongoose');
+
+const registrationSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+const Registration = mongoose.model('Registration', registrationSchema);
+
+module.exports = Registration;
