9f7ba51471fa2bb994191e5a55ed93a1d4fa261f
Narendra Chitteti <122769756+narendrachitteti@users.noreply.github.com>
2023-12-22 13:04:13 +0530

    hii

--- Controllers/formController.js
+++ Controllers/formController.js
@@ -1,4 +1,3 @@
-
 const Fordata = require('../models/formDataModel');
 
 // CRUD operations
@@ -22,19 +21,6 @@ exports.readAllDocuments = async (req, res) => {
 };
 
 exports.updateDocument = async (req, res) => {
-async function getProducts(req, res) {
-  try {
-    // Fetch all products from the database
-    const products = await FormDatas.find({}, 'product'); // Fetching only the 'product' field
-
-    res.json(products);
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    res.status(500).send('Internal Server Error');
-  }
-}
-
-async function updateFormData(req, res) {
   try {
     const document = await Fordata.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(document);
@@ -53,31 +39,18 @@ exports.deleteDocument = async (req, res) => {
 };
 
 exports.getDistinct = async (req, res) => {
-    try {
-      const distinctCompanies = await Fordata.distinct('Company');
-      const distinctHsn = await Fordata.distinct('HmsCode'); 
-      
-      const distinctValues = {
-        companies: distinctCompanies,
-        hsn: distinctHsn,
-      };
-  
-      res.json(distinctValues);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
+  try {
+    const distinctCompanies = await Fordata.distinct('Company');
+    const distinctHsn = await Fordata.distinct('HmsCode'); 
+    
+    const distinctValues = {
+      companies: distinctCompanies,
+      hsn: distinctHsn,
+    };
 
-    res.json(updatedData);
+    res.json(distinctValues);
   } catch (error) {
-    console.error('Error updating data:', error);
+    console.error('Error fetching distinct values:', error);
     res.status(500).send('Internal Server Error');
   }
-}
-
-module.exports = {
-  createFormData,
-  getFormData,
-  updateFormData,
-  getProducts,
 };
