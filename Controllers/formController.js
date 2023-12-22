97a82560e37b1c5525405102032296025d878ee6
KarthikThakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2023-12-22 13:46:55 +0530

    hiii

--- Controllers/formController.js
+++ Controllers/formController.js
@@ -39,31 +39,18 @@ exports.deleteDocument = async (req, res) => {
 };
 
 exports.getDistinct = async (req, res) => {
-    try {
-      const distinctCompanies = await Fordata.distinct('Company');
-      const distinctHsn = await Fordata.distinct('HmsCode'); 
-      
-      const distinctValues = {
-        companies: distinctCompanies,
-        hsn: distinctHsn,
-      };
-  
-      res.json(distinctValues);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
+  try {
+    const distinctCompanies = await Fordata.distinct('Company');
+    const distinctHsn = await Fordata.distinct('HmsCode'); 
+    
+    const distinctValues = {
+      companies: distinctCompanies,
+      hsn: distinctHsn,
+    };
 
-    res.json(updatedData);
+    res.json(distinctValues);
   } catch (error) {
-    console.error('Error updating data:', error);
+    console.error('Error fetching distinct values:', error);
     res.status(500).send('Internal Server Error');
   }
-}
-
-module.exports = {
-  createFormData,
-  getFormData,
-  updateFormData,
-  getProducts,
 };
