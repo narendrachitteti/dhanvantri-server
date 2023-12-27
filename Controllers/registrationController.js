e3b93b590aa86c424d379c35b7ac310decba8f11
Gurunath <144308289+GurunathBugga@users.noreply.github.com>
2023-12-27 14:00:18 +0530

    hi

--- Controllers/registrationController.js
+++ Controllers/registrationController.js
@@ -2,15 +2,15 @@ const Registration = require('../models/registrationModel');
 
 exports.registerUser = async (req, res) => {
   try {
-    const { username, email, password, name, staffid } = req.body;
+    const { username, email, password } = req.body;
 
     // Validate data (add more validation as needed)
-    if (!username || !email || !password || !name || !staffid) {
+    if (!username || !email || !password ) {
       return res.status(400).json({ message: 'Please provide all required fields.' });
     }
 
     // Save data to MongoDB
-    const newRegistration = new Registration({ username, email, password, name, staffid });
+    const newRegistration = new Registration({ username, email, password });
     await newRegistration.save();
 
     res.status(201).json({ message: 'Registration successful.', user: newRegistration });
