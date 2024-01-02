7185195277572feffc93c9a2ff87939c3767b20c
KarthikThakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2024-01-02 20:46:07 +0530

    hiii

--- models/PatientBillModel.js
+++ models/PatientBillModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const patientBillSchema = new mongoose.Schema({
   patientName: String,
   mobilenumber: String,
+  doctorName:String,
   date: String,
   items: Array,
   subtotalWithGST: Number,
